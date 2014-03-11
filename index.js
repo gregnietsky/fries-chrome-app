06a32996e34442786813cb9882ae859ffcbcd075
Homer Simpson <gregnietsky@gmail.com>
2014-03-11 10:14:23 +0200

    Remove testing code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function onBoundsChanges() {
-  var div = document.getElementById('testdiv');
   var webview = document.getElementById('appwebview');
   if (webview !== null) {
     webview.style.height = document.documentElement.clientHeight + 'px';
